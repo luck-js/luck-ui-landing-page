2f2dafc0de5b2e5c5d3a9af6d35a3b80b87dcb0e
Daniel Karski <daniel.karski5q@gmail.com>
2019-09-03 15:57:14 +0200

    Make bubble svg component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import styled, {keyframes} from 'styled-components';
 import Layout from '../components/Layout';
-import {helper, getRandomInt} from "../utils/helper"
+import { helper, getRandomInt } from '../utils/helper';
 
 const CONTAINER_HEIGHT = 160;
+if (typeof window === 'undefined') {
+  // @ts-ignore
+  global.window = {};
+}
+const innerWidth = window.innerWidth;
 
 const makeTravel = (x: number) => keyframes`
-  from { left: ${x}%; }
-  to   { left: ${x > 0 ? x - 200  : x + 200}%; }
+  from { transform: translateX(${x > 0 ? x : x + innerWidth}px); }
+  to { transform: translateX(${x < 0 ? x : x + innerWidth}px); }
 `;
 
 const makeBounce = (y: number) => keyframes`
   from, to  {
-    bottom: ${y}px;
+    transform: translateY(${y}px);
   }
   50% {
-    bottom: ${y > 0 ? y - CONTAINER_HEIGHT + 20  : y + CONTAINER_HEIGHT - 20}px; 
+    transform: translateY(${CONTAINER_HEIGHT}px);
   }
 `;
 
-const Container = styled('div')`
+const Container = styled('svg')`
   position: fixed;
   bottom: 0;
   width: 100%;
@@ -28,16 +33,16 @@ const Container = styled('div')`
 `;
 
 interface Bubble {
-  time: number
-  size: number
-  opacity: number
+  time: number;
+  size: number;
+  opacity: number;
   position: {
     x: number;
     y: number;
-  }
+  };
 }
 
-const Bubble = ({ time, ...props}: Bubble) => {
+const Bubble = ({ time, ...props }: Bubble) => {
   const travelerTime = time * 100;
   const bounceTime = time * 96;
   return (
@@ -46,49 +51,40 @@ const Bubble = ({ time, ...props}: Bubble) => {
     </Bubble.Traveler>
   );
 };
-
-Bubble.Traveler = styled('div')<Bubble>`
-  top: ${CONTAINER_HEIGHT / 4}px;
-  position: absolute;
-  width: ${props => props.size + "px"};
-  height: ${CONTAINER_HEIGHT}px;
-
-  animation: ${props => makeTravel(props.position.x)} ${props => props.time + "ms"} linear infinite alternate;
+//
+Bubble.Traveler = styled('g')<Bubble>`
+  animation: ${props => makeTravel(props.position.x)} ${props => props.time + 'ms'} linear infinite
+    alternate;
 `;
 
-Bubble.Bouncer = styled('div')<Bubble>`
-  position: absolute;
-  width: ${props => props.size + "px"};
-  height: ${props => props.size + "px"};
-  background: white;
-  opacity: ${props => props.opacity * 1};
-  border-radius: 50%;
-
+Bubble.Bouncer = styled('circle')<any>`
+  fill: white;
+  fill-opacity: ${props => props.opacity};
+  r: ${props => props.size / 2};
+  cy: ${props => props.position.y}px;
   animation: ${props => makeBounce(props.position.y)} ${props => props.time + "ms"} infinite;
 `;
 
 const Index: React.FunctionComponent = () => {
-  if (typeof window === 'undefined') {
-    // @ts-ignore
-    global.window = {}
-  }
-  const timeRatio = window.innerWidth / 18;
-  const bubbles = helper(0, 1000)
+  const timeRatio = innerWidth / 10;
+  const bubbles = helper(0, 1000);
   return (
     <Layout title="Home">
-      <Container>
-        {
-          bubbles.map(bubbles => {
-            const time = getRandomInt(timeRatio - 6, timeRatio + 6)
-            const size = getRandomInt(40, 60);
-            const position = {
-              x: getRandomInt(-100, 100),
-              y: getRandomInt(-CONTAINER_HEIGHT + 20, CONTAINER_HEIGHT - 20),
-            }
-            const opacity = getRandomInt(60, 100) / 100
-            return <Bubble key={bubbles} time={time} size={size} position={position} opacity={opacity} />
-          })
-        }
+      <Container width="100%" height={`${CONTAINER_HEIGHT}px`}>
+
+
+        {bubbles.map(bubbles => {
+          const time = getRandomInt(timeRatio - 6, timeRatio + 6);
+          const size = getRandomInt(40, 60);
+          const position = {
+            x: getRandomInt(-200, innerWidth + 200),
+            y: getRandomInt(size, CONTAINER_HEIGHT),
+          };
+          const opacity = getRandomInt(60, 100) / 100;
+          return (
+            <Bubble key={bubbles} time={time} size={size} position={position} opacity={opacity} />
+          );
+        })}
       </Container>
     </Layout>
   );
