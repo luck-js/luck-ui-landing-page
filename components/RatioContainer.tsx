80cdbaed6c5385e78a1102beb722b5b095b44296
Daniel Karski <daniel.karski5q@gmail.com>
2019-09-20 10:28:02 +0200

    Added Ratio Container

---
+++ components/RatioContainer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import {Box} from "../utils/Box"
+import {Flex} from "../utils/Flex"
+
+
+const RatioContainer: React.FunctionComponent<any> = ({
+  children,
+  ratio = "100%",
+  padding = 0,
+  ...props
+}) => {
+  const Container = styled(Box)`
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 100%;
+    padding: ${padding};
+    transform: translateY(-50%);
+  `;
+
+  const RatioKeeper = styled(Flex)`
+    overflow: hidden;
+    position: relative;
+    height: 0;
+    padding-top: ${ratio};
+    width: 100%;
+  `;
+
+  return (
+    <RatioKeeper {...props}>
+      <Container>{children}</Container>
+    </RatioKeeper>
+  );
+};
+
+export default RatioContainer;
