724343d8e9fcf27a41098d7fd3d8a6ec8adadcbf
Daniel Karski <daniel.karski5q@gmail.com>
2019-11-07 17:10:44 +0100

    Added Interface for typography styles

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,17 @@
 import styled, { css } from 'styled-components';
-import { applyStyleModifiers } from 'styled-components-modifiers';
+import {applyStyleModifiers, ModifierKeys, ModifiersConfig} from 'styled-components-modifiers';
 import { BaseButton } from './BaseButton';
 import * as Theme from '../../utils/theme';
+import {TextStyle} from "../../utils"
 
-const BUTTON_VARIANTS = {
+interface ButtonProps extends TextStyle{
+  modifiers?:ModifierKeys;
+  href?: string;
+  ariaLabel: string;
+
+}
+
+const BUTTON_VARIANTS:ModifiersConfig  = {
   black: () => css`
     color: ${Theme.colors.black};
     
@@ -13,7 +21,8 @@ const BUTTON_VARIANTS = {
   `,
 };
 
-export const Button = styled(BaseButton)<any>`
+
+export const Button = styled(BaseButton)<ButtonProps>`
   font-weight: 700;
   letter-spacing: 0;
   cursor: pointer;
