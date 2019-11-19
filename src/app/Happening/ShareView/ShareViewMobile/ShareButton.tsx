8fd4eb1243d19c468f86bc0150cef4b409dafae6
Daniel Karski <daniel.karski5q@gmail.com>
2019-11-19 09:34:02 +0100

    Updated share icon

--- src/app/Happening/ShareView/ShareViewMobile/ShareButton.tsx
+++ src/app/Happening/ShareView/ShareViewMobile/ShareButton.tsx
@@ -4,13 +4,13 @@ import { BaseButton } from '../../../../components';
 import { Theme } from '../../../../utils';
 import { Element } from '../../shared';
 // @ts-ignore
-import Copy from '../../../../../static/copy.svg';
+import Share from '../../../../../static/share.svg';
 
 const ShareButton = ({ children, onClick }: any) => {
   return (
     <ShareButton.Button onClick={onClick} onMouseDown={(e: any) => e.preventDefault()}>
       <Element.Text>{children}</Element.Text>
-      <Copy />
+      <Share />
     </ShareButton.Button>
   );
 };
@@ -20,11 +20,11 @@ ShareButton.Button = styled(Element.Container).attrs({ as: BaseButton })`
   svg {
     position: absolute;
     top: 50%;
-    margin-top: -7.5px;
-    right: 15px;
-    width: 15px;
+    margin-top: -10px;
+    right: 10px;
+    width: 20px;
     text-align: center;
-    height: 15px;
+    height: 20px;
     display: block;
     path {
       fill: ${Theme.colors.main};
