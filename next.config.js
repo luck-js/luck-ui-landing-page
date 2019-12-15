55079401da5d38370a308a395c6f04f90fcf384f
Daniel Karski <daniel.karski5q@gmail.com>
2019-12-15 10:13:30 +0100

    POC

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withTypescript = require('@zeit/next-typescript')
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
 module.exports = withTypescript({
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
