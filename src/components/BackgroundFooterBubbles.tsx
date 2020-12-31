af63f9f16365d129887238e1999ce39de399b96e
Daniel Karski <daniel.karski5q@gmail.com>
2020-12-31 11:44:28 +0100

    Added arrow to welcome section

--- src/components/BackgroundFooterBubbles.tsx
+++ src/components/BackgroundFooterBubbles.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import styled from 'styled-components';
+// @ts-ignore
+import WideArrow from '../../static/wide-arrow.svg';
+import { Theme } from '../utils';
 import media from '../utils/media';
 import { Box } from './Box';
 import { LazyImage } from './LazyImage';
 
-const Container = styled(Box)`
+const Wrapper = styled(Box)`
   z-index: 0;
- 
+
   pointer-events: none;
   width: 100%;
   height: auto;
@@ -14,10 +17,12 @@ const Container = styled(Box)`
   bottom: 0;
   left: 0;
   line-height: 0;
-   
+`;
+
+const Container = styled(Box)`
   ${media.greaterThan('mobile')`
     width: auto;
-    height: 134px;
+    height: 180px;
   `}
   
    ${media.greaterThan('tablet')`
@@ -25,7 +30,7 @@ const Container = styled(Box)`
   `}
   
   ${media.greaterThan('desktop')`
-    height: 192px;
+    height: 207px;
     width: 100%;
     
     img {
@@ -62,21 +67,45 @@ const BackgroundFooterBubblesImage = (props: { src: string; placeholderSrc: stri
   </LazyImage>
 );
 
+const Arrow = styled(WideArrow)`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  width: 42px;
+  height: auto;
+  transform: translateX(-50%) translateY(0) rotate(90deg);
+  cursor: pointer;
+  transition: transform 0.5s, opacity 0.5s;
+  pointer-events: all;
+
+  &:hover {
+    transform: translateX(-50%) translateY(5px) rotate(90deg);
+    opacity: 0.8;
+  }
+
+  path {
+    stroke: ${Theme.colors.mainContrast};
+  }
+`;
+
 export const BackgroundFooterBubbles = () => {
   return (
-    <>
-      <Container display={['inline-block', 'none']}>
+    <Wrapper>
+      <Container display={['block', 'none']}>
         <BackgroundFooterBubblesImage
           src={'/static/bg-narrow-bubbles.png'}
           placeholderSrc={'/static/compr-bg-narrow-bubbles.png'}
         />
       </Container>
-      <Container display={['none', 'inline-block']}>
+      <Container display={['none', 'block']}>
         <BackgroundFooterBubblesImage
           src={'/static/bg-footer-bubbles.png'}
           placeholderSrc={'/static/compr-bg-footer-bubbles.png'}
         />
       </Container>
-    </>
+      <a href="#description-section">
+        <Arrow />
+      </a>
+    </Wrapper>
   );
 };
