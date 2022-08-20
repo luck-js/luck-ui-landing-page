74ed8608a76db3bcdbac4adabebf1a692511ad4f
Daniel Karski <daniel.karski5q@gmail.com>
2022-08-20 11:37:28 +0200

    Updated path for images vol.3

--- src/app/BubblesNarrowBackground.tsx
+++ src/app/BubblesNarrowBackground.tsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const BubblesNarrowBackground = styled('div')`
   z-index: 0;
-  background-image: url("../../public/static/bg-narrow-bubbles.png");
+  background-image: url("/static/bg-narrow-bubbles.png");
   pointer-events: none;
 `;
