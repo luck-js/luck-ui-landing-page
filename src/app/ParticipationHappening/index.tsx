2bb777ea081d82f826a114d3307b27af2cc89a65
Daniel Karski <daniel.karski5q@gmail.com>
2022-08-27 11:04:12 +0200

    `BannerImage` moved to parent route

--- src/app/ParticipationHappening/index.tsx
+++ src/app/ParticipationHappening/index.tsx
@@ -4,10 +4,10 @@ import WelcomeMemberSection, { WelcomeMemberSectionData } from './WelcomeMemberS
 import MatchedMemberSection from './MatchedMemberSection';
 import { MatchedMemberSectionData } from './MatchedMemberSection';
 import styled from 'styled-components';
-import { Box, Flex, NAVIGATION_SHADOW } from '../../components';
+import { Box, Flex, LazyImage, NAVIGATION_SHADOW } from '../../components';
 import { Theme } from '../../utils';
 import { BackgroundFooterBubbles } from '../../components/BackgroundFooterBubbles';
-import { event } from 'nextjs-google-analytics';
+import media from '../../utils/media';
 
 export interface ParticipationHappeningViewData {
   welcomeMember: WelcomeMemberSectionData;
@@ -18,6 +18,39 @@ interface ParticipationHappeningViewProps {
   data: ParticipationHappeningViewData;
   analytics: any;
 }
+
+const Image = styled('img')<{ loading: any }>`
+  width: 100%;
+  height: auto;
+  filter: ${(props) => (props.loading ? 'blur(10px)' : 'blur(0)')};
+  opacity: ${(props) => (props.loading ? 0.8 : 0.95)};
+  cursor: pointer;
+  transition: opacity 0.5s, all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+    opacity: 1;
+  }
+  ${media.greaterThan('mobile')`
+    width: 100%;
+    max-width: 450px;
+    height: auto;
+  `}
+
+  ${media.greaterThan('tablet')`
+    
+  `}
+  
+  ${media.greaterThan('desktop')`
+  
+  `}
+`;
+
+const BannerImage = (props: { src: string; placeholderSrc: string }) => (
+  <LazyImage useSensor={false} {...props}>
+    {(src: any, loading: boolean) => <Image src={src} alt={loading.toString()} loading={loading} />}
+  </LazyImage>
+);
 
 const Index = ({ data, analytics }: ParticipationHappeningViewProps) => {
   const [shouldShowMatchedMember, SetShouldShowMatchedMember] = useState(false);
@@ -34,21 +67,24 @@ const Index = ({ data, analytics }: ParticipationHappeningViewProps) => {
     <Index.Container>
       <Background />
       {!shouldShowMatchedMember && (
-        <Index.WelcomeMemberSectionContainer>
-          <WelcomeMemberSection data={data.welcomeMember} onClick={handleOnClick} />
-        </Index.WelcomeMemberSectionContainer>
+        <WelcomeMemberSection data={data.welcomeMember} onClick={handleOnClick} />
       )}
-      {shouldShowMatchedMember && (
-        <Index.MatchedMemberSectionContainer>
-          <MatchedMemberSection data={data.matchedMember} handleBannerClick={handleBannerClick} />
-        </Index.MatchedMemberSectionContainer>
-      )}
+      {shouldShowMatchedMember && <MatchedMemberSection data={data.matchedMember} />}
+      <Index.BannerImageContainer>
+        <a
+          href="https://palisienaturalnie.pl/pl/c/Swiece/16"
+          target="_blank"
+          onClick={handleBannerClick}
+        >
+          <BannerImage src={'/static/banner.png'} placeholderSrc={'/static/compr-banner.jpg'} />
+        </a>
+      </Index.BannerImageContainer>
       <BackgroundFooterBubbles />
     </Index.Container>
   );
 };
 
-Index.Container = styled(Box)`
+Index.Container = styled(Flex)`
   position: relative;
   padding: ${Theme.space.xregular - NAVIGATION_SHADOW}px ${Theme.space.small}px 200px
     ${Theme.space.small}px;
@@ -56,17 +92,16 @@ Index.Container = styled(Box)`
   text-align: center;
   background-color: ${Theme.colors.mainContrast};
   min-height: 100%;
-  display: grid;
-`;
 
-Index.WelcomeMemberSectionContainer = styled(Flex)`
   justify-content: center;
   flex-direction: column;
-  min-height: 100%;
 `;
-
-Index.MatchedMemberSectionContainer = styled(Box)`
-  display: grid;
+Index.BannerImageContainer = styled(Box)`
+  position: absolute;
+  z-index: 999;
+  left: 50%;
+  bottom: 15px;
+  transform: translateX(-50%);
 `;
 
 export default Index;
