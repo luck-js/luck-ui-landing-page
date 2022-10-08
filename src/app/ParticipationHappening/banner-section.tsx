b9b91c1e295efea2582ab1cb09c4c59918ddd217
Daniel Karski <daniel.karski5q@gmail.com>
2022-10-08 10:12:34 +0200

    Implement `getBannerItem` logic

--- src/app/ParticipationHappening/banner-section.tsx
+++ src/app/ParticipationHappening/banner-section.tsx
@@ -2,6 +2,41 @@ import React from 'react';
 import BannerItem, { Banner, BannerType } from './banner-item';
 import { Box } from '../../components';
 
+interface BannerItemFactorsMap {
+  [key: string]: number;
+}
+
+let defaultBannerItemFactors: {
+  [key in BannerType]: number;
+} = {
+  [BannerType.Palisienaturalnie]: 0.5,
+  [BannerType.Podstawka]: 0.5,
+};
+
+const getBannerItemFactors = (factors: string | undefined = ''): BannerItemFactorsMap => {
+  try {
+    const json = JSON.parse(factors);
+    return json[BannerType.Palisienaturalnie] ? json : json;
+  } catch {
+    return defaultBannerItemFactors;
+  }
+};
+
+const bannerItemFactors = getBannerItemFactors(process.env.BANNER_ITEM_FACTORS);
+
+const getBannerItem = (bannerItems: BannerItemProps[]): BannerItemProps => {
+  const bannerItemFactorsSorted: string[] = Object.keys(bannerItemFactors).sort((key) => {
+    const filteredFactorKeys = Object.keys(bannerItemFactors).filter((k) => k !== key);
+
+    const r =
+      filteredFactorKeys.reduce((prev, filteredFactorKey) => {
+        return prev + bannerItemFactors[filteredFactorKey];
+      }, 0) / filteredFactorKeys.length;
+    return r - Math.random()
+  });
+  return bannerItems.find((item) => item.desktop.type === bannerItemFactorsSorted[0]) ?? bannerItems[0];
+};
+
 interface BannerItemProps {
   mobile: Banner;
   desktop: Banner;
@@ -13,7 +48,7 @@ interface BannerSectionProps {
 }
 
 const BannerSection = ({ bannerItems, onBannerClick }: BannerSectionProps) => {
-  const bannerItem = bannerItems[1];
+  const bannerItem = getBannerItem(bannerItems);
 
   return (
     <>
